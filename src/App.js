25b9ff111f0add9f31a473ad70feec7be1b30c44
Yura <yuras92@ukr.net>
2024-02-01 13:48:27 +0200

    added a task condition

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+
+/*
+2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
+https://rickandmortyapi.com/
+https://rickandmortyapi.com/api/character
+Створити 6 персонажів
+*/
+
 import React from 'react';
 import {Character} from "./components/character";
 
