72e389ce944841330add54b38e5d85436ad62e2b
Yura <yuras92@ukr.net>
2024-02-01 13:12:38 +0200

    add secondTask

---
+++ src/components/character.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Character = ({characteristic}) => {
+    const {id, name, status, species, type, gender, origin, location, image, episode, url, created} = characteristic
+    return (
+        <div>
+            <h1>{id} {name}</h1>
+            <img src={image} alt={name}/>
+            <h2>Status: {status}</h2>
+            <h2>Species: {species}</h2>
+            <h2>Type: {type}</h2>
+            <h2>Gender: {gender}</h2>
+            <h2>OriginName:{origin.name} <br/> OriginURL: <a href={origin.url}>{origin.url}</a></h2>
+            <h2>LocationName:{location.name} <br/> LocationURL: <a href={location.url}>{location.url}</a></h2>
+            <h2>Episodes:</h2>
+            <ul>
+                {episode.map((ep, index) => (
+                    <li key={index}>
+                        <a href={ep}>{ep}</a>
+                    </li>
+                ))}
+            </ul>
+            <h2>URL: <a href={url}>{url}</a></h2>
+            <h2>Created: {created}</h2>
+        </div>
+    );
+};
+
+export {Character};
