f40285b31009eb5f7013f773a34a55093bddf602
Yura <yuras92@ukr.net>
2024-02-09 15:08:25 +0200

    add validation

--- src/components/comment/CommentFormComponent.js
+++ src/components/comment/CommentFormComponent.js
@@ -4,23 +4,35 @@ import styles from './CommentFormComponent.module.css';
 import { commentService } from "../../services/commentService";
 
 const CommentFormComponent = ({ setComments }) => {
-    const { reset, handleSubmit, register } = useForm();
+    const { reset, formState: { isValid }, handleSubmit, register } = useForm({
+        mode: "all"
+    });
 
-    const save = (comment) => {
-        commentService.post(comment)
-            .then(({data}) => {
-                setComments(prevComments => [...prevComments, data]);
-                reset();
-            })
+    const save = async (comment) => {
+        try {
+            const { data } = await commentService.post(comment);
+            setComments(prevComments => [...prevComments, data]);
+            reset();
+        } catch (error) {
+            console.error("Error while saving comment:", error);
+        }
     };
 
     return (
         <form className={styles.postContainer} onSubmit={handleSubmit(save)}>
-            <input type="text" placeholder={'postId'} {...register('postId')} />
-            <input type="text" placeholder={'name'} {...register('name')} />
-            <input type="text" placeholder={'email'} {...register('email')} />
-            <input type="text" placeholder={'body'} {...register('body')} />
-            <button type="submit">Save</button>
+            <input type="text" placeholder={'name'} {...register('name', {
+                required: true,
+                pattern: /^[a-zA-Z '.-]*$/,
+            })} />
+            <input type="text" placeholder={'email'} {...register('email', {
+                required: true,
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })} />
+            <input type="text" placeholder={'body'} {...register('body', {
+                required: true,
+                pattern: /^.{1,100}$/,
+            })} />
+            <button type="submit" disabled={!isValid}>Save</button>
         </form>
     );
 };
--- src/components/user/UserFormComponent.js
+++ src/components/user/UserFormComponent.js
@@ -4,22 +4,36 @@ import { userService } from "../../services/userService";
 import styles from './UserFormComponent.module.css'
 
 const UserFormComponent = ({ setUsers }) => {
-    const { reset, register, handleSubmit } = useForm();
+    const { reset, formState: { isValid }, register, handleSubmit } = useForm({
+        mode: "all"
+    });
 
-    const save = (user) => {
-        userService.post(user)
-            .then(({data}) => {
-                setUsers(prevUsers => [...prevUsers, data]);
-                reset();
-            })
+    const save = async (user) => {
+        try {
+            const { data } = await userService.post(user);
+            setUsers(prevUsers => [...prevUsers, data]);
+            reset();
+        } catch (error) {
+            console.error("Error while saving user:", error);
+        }
     };
 
     return (
         <form className={styles.form} onSubmit={handleSubmit(save)}>
-            <input type="text" placeholder={'name'} {...register('name')} />
-            <input type="text" placeholder={'username'} {...register('username')} />
-            <input type="text" placeholder={'email'} {...register('email')} />
-            <button>Save</button>
+            <input type="text" placeholder={'name'} {...register('name', {
+                required: true,
+                pattern: /^[a-zA-Z '.-]*$/
+            })} />
+            <input type="text" placeholder={'username'} {...register('username', {
+                required: true,
+                pattern: /^[a-zA-Z0-9 '.-]*$/
+            })} />
+            <input type="text" placeholder={'email'} {...register('email', {
+                required: true,
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            })} />
+
+            <button disabled={!isValid}>Save</button>
         </form>
     );
 };
